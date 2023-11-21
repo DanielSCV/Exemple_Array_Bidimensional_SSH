e609e6a1d2ca04965d9d45cf8a6d90d6dcc6994f
Daniel Cardenas <dcv00@iesemilidarder.com>
2023-11-21 11:40:09 +0100

    Segunda versión, añadido propiedad intelectual y fichero README

--- Fibonaci.java
+++ Fibonaci.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Fibonaci{
 
     public static void main(String[] args){
- 
+        
         Scanner sc = new Scanner(System.in);
         int numero,fibo1,fibo2,i;
   
@@ -22,7 +22,16 @@ public class Fibonaci{
             System.out.print(fibo2 + " ");
             fibo2 = fibo1 + fibo2;
             fibo1 = fibo2 - fibo1;
+        
+        
         }
+        String nombreCompleto = "Daniel Santiago";
+        
+        String mensajePropiedad = "Propiedad de " + nombreCompleto;
+        String mensajeIES = "IES Emili Darder 2023";
+        
+        System.out.println(mensajePropiedad);
+        System.out.println(mensajeIES);
         System.out.println();
     }
 }
